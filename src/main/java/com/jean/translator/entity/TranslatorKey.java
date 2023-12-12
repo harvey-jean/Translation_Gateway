62fd6ef70206bd6a9a3cb3837af1678c4f0804de
harvey-jean <ihevea@gmail.com>
2023-12-12 06:30:31 +0100

    #5:Refactored TranslatorKey

--- src/main/java/com/jean/translator/entity/TranslatorKey.java
+++ src/main/java/com/jean/translator/entity/TranslatorKey.java
@@ -2,4 +2,8 @@ package com.jean.translator.entity;
 
 
 public record TranslatorKey(int id, String language) {
+
+    public static TranslatorKey create(int id, String language){
+        return new TranslatorKey(id, language);
+    }
 }
--- src/main/java/com/jean/translator/repository/TranslatorDataProvider.java
+++ src/main/java/com/jean/translator/repository/TranslatorDataProvider.java
@@ -9,17 +9,17 @@ public class TranslatorDataProvider {
     public static final Map<TranslatorKey, String> translationsStore = new HashMap<>();
 
     static {
-        translationsStore.put(new TranslatorKey(1,"en"), "Cat");
-        translationsStore.put(new TranslatorKey(1,"de"), "Katze");
-        translationsStore.put(new TranslatorKey(1,"it"), "Gatto");
+        translationsStore.put(TranslatorKey.create(1,"en"), "Cat");
+        translationsStore.put(TranslatorKey.create(1,"de"), "Katze");
+        translationsStore.put(TranslatorKey.create(1,"it"), "Gatto");
 
-        translationsStore.put(new TranslatorKey(2,"en"), "Dog");
-        translationsStore.put(new TranslatorKey(2,"de"), "Hund");
-        translationsStore.put(new TranslatorKey(2,"it"), "Cane");
+        translationsStore.put(TranslatorKey.create(2,"en"), "Dog");
+        translationsStore.put(TranslatorKey.create(2,"de"), "Hund");
+        translationsStore.put(TranslatorKey.create(2,"it"), "Cane");
 
-        translationsStore.put(new TranslatorKey(3,"en"), "Horse");
-        translationsStore.put(new TranslatorKey(3,"de"), "Pferd");
-        translationsStore.put(new TranslatorKey(3,"it"), "Cavallo");
+        translationsStore.put(TranslatorKey.create(3,"en"), "Horse");
+        translationsStore.put(TranslatorKey.create(3,"de"), "Pferd");
+        translationsStore.put(TranslatorKey.create(3,"it"), "Cavallo");
     }
 
 }
